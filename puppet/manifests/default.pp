dad9cb9e9628c5723f6de54e79f550c8c695bb8d
Marion <my4skcg@gmail.com>
2013-06-13 00:48:00 -0500

    Added bootstrap; default.pp-comment out puphpet stuff

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -15,19 +15,19 @@ package { ['build-essential', 'python-software-properties',
     require => Exec['apt-get update'],
 }
 
-file { '/home/vagrant/.bash_aliases' :
-    source => 'puppet:///modules/puphpet/dot/.bash_aliases',
-    ensure => 'present',
-}
+#file { '/home/vagrant/.bash_aliases' :
+#    source => 'puppet:///modules/puphpet/dot/.bash_aliases',
+#    ensure => 'present',
+#}
 
 apt::ppa { 'ppa:ondrej/php5' :
     before  => Class['php']
 }
 
-git::repo { 'puphpet' :
-    path   => '/home/vagrant/shared/puphpet.dev/',
-    source => 'https://github.com/jtreminio/Puphpet.git'
-}
+#git::repo { 'puphpet' :
+#    path   => '/home/vagrant/shared/puphpet.dev/',
+#    source => 'https://github.com/jtreminio/Puphpet.git'
+#}
 
 class { 'apache' :
     require => Apt::Ppa['ppa:ondrej/php5'],
@@ -39,15 +39,15 @@ apache::dotconf { 'custom' :
 
 apache::module { 'rewrite' : }
 
-apache::vhost { 'puphpet' :
-    server_name   => 'puphpet.dev',
-    serveraliases => ['www.puphpet.dev',],
-    docroot       => '/home/vagrant/shared/puphpet/web',
-    port          => '80',
-    env_variables => { 'APP_ENV' => 'dev' },
-    priority      => '1',
-    require       => Git::Repo['puphpet']
-}
+#apache::vhost { 'puphpet' :
+#    server_name   => 'puphpet.dev',
+#    serveraliases => ['www.puphpet.dev',],
+#    docroot       => '/home/vagrant/shared/puphpet/web',
+#    port          => '80',
+#    env_variables => { 'APP_ENV' => 'dev' },
+#    priority      => '1',
+#    require       => Git::Repo['puphpet']
+#}
 
 class { 'git' :
     svn => true,
@@ -93,15 +93,15 @@ xdebug::config { 'cli' : }
 
 class { 'php::composer': }
 
-php::composer::run { 'puphpet':
-    path    => '/home/vagrant/shared/puphpet.dev/',
-    require => Git::Repo['puphpet']
-}
+#php::composer::run { 'puphpet':
+#    path    => '/home/vagrant/shared/puphpet.dev/',
+#    require => Git::Repo['puphpet']
+#}
 
 apache::vhost { 'mww' :
     server_name   => 'mww.dev',
     serveraliases => ['www.mww.dev',],
-    docroot       => '/home/vagrant/shared/mww',
+    docroot       => '/home/vagrant/shared',
     port          => '80',
     priority      => '1'
 }
